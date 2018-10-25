02a1530c9f3c1bb95acd41d0039dc7041a0fd728
Frankydot <frankmwangin@gmail.com>
2018-10-25 09:03:16 +0300

    added the get route to allow display

--- src/main/java/leaderboard.java
+++ src/main/java/leaderboard.java
@@ -1,6 +1,13 @@
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
 import static spark.Spark.setPort;
 import static spark.Spark.staticFileLocation;
 import static spark.debug.DebugScreen.enableDebugScreen;
+
 
 public class leaderboard {
         public static void main(String[] args) {
@@ -16,6 +23,12 @@ public class leaderboard {
             String layout = "templates/layout.vtl";
             staticFileLocation ("/public");
             enableDebugScreen();
+
+            get("/", (request, response) -> {
+                Map<String, Object> model = new HashMap<String, Object>();
+                model.put("template", "/templates/home.vtl");
+                return new ModelAndView(model, layout);
+            }, new VelocityTemplateEngine());
     }
 
 }
